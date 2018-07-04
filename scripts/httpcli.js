9f843c6b7c6c6bc2f4fb8e80ea92c53ba006f727
Fendo181 <fendo181git@gmail.com>
2018-07-04 09:21:11 +0900

    cheerio-httpcliを使って勤之助にアクセスできることを確認した

--- scripts/httpcli.js
+++ scripts/httpcli.js
@@ -1,17 +1,9 @@
-var client = require('cheerio-httpcli');
+const client = require('cheerio-httpcli');
 
-// Googleで「node.js」について検索する。
-var word = 'node.js';
+//勤之助にアクセスする
+let url = 'https://www.4628.jp/?module=top&rd=1';
 
-client.fetch('http://www.google.com/search', { q: word }, function (err, $, res, body) {
+client.fetch(url, function (err, $, res, body) {
   // レスポンスヘッダを参照
-  console.log(res.headers);
-
-  // HTMLタイトルを表示
-  console.log($('title').text());
-
-  // リンク一覧を表示
-  $('a').each(function (idx) {
-    console.log($(this).attr('href'));
-  });
+  console.log(res);
 });
